a3b9b74f7d9e5b216b06b4443a60a47acabe4c0a
koouu <kotatata420@gmail.com>
2022-03-17 16:34:58 +0900

    bakery search

--- src/pages/search.js
+++ src/pages/search.js
@@ -1,10 +1,17 @@
 import React, { useEffect, useState } from 'react'
 import { ActivityIndicator,FlatList,StyleSheet, Button, Text, View,TextInput  } from 'react-native'
+import { collection, doc ,onSnapshot} from 'firebase/firestore';
+
+import { firebase } from '../firebase/firebase';
+
 
 const SearchScreen = ({ navigation }) => {
 	const [isLoading, setLoading] = useState(true);
   const [data, setData] = useState([]);
 	const [postcode,setPostcode]=useState('');
+	const [bakeries, setBakeries] = useState([]);
+	const [nearbakeries, setNearBakeries] = useState([]);
+	var address='';
 
   const getPost = async () => {
      try {
@@ -15,25 +22,51 @@ const SearchScreen = ({ navigation }) => {
 			console.log(json.results);
 			console.log(json.results[0].address1);
 			console.log(json.results);
+			address=json.results[0].address1+json.results[0].address2;
+			console.log(address);
+			console.log("log");
+			console.log(json.results[0].address1+json.results[0].address2);
+
+			getBakery()
     } catch (error) {
       console.error(error);
     } finally {
       setLoading(false);
     }
   }
-	/*
+	const getBakery= async () => {
+		nearbakeries.length=0;
+		for(var i=0;i<bakeries.length;i++){
+			
+			const bakery_post = await fetch('https://zipcloud.ibsnet.co.jp/api/search?zipcode='+bakeries[i].postcode);
+			const json_bakery = await bakery_post.json();
+			console.log("get");
+			console.log(json_bakery)
+			console.log(json_bakery.results[0].address1+json_bakery.results[0].address2);
+			
+			
+			if(address==(json_bakery.results[0].address1+json_bakery.results[0].address2)){
+				setNearBakeries([...nearbakeries,{name:bakeries[i].name}])
+				console.log(bakeries[i].postcode);
+				console.log(nearbakeries);
+			}
+			console.log(bakeries[i].name);
+		}
+	}
+
   useEffect(() => {
-    getPost();
+    firebase.firestore().collection('bakery').get().then((querySnapshot) => {
+      setBakeries(
+        querySnapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
+      );
+    });
+
   }, []);
-	*/
 	
 	return (
 		<View style={styles.container}>
 			
-			{isLoading ? (<Text>住所が出ます</Text>) : (
-        
-        <Text>{data[0].address1}{data[0].address2}</Text>
-      )}
+			
 			<TextInput 
 				placeholder='郵便番号'
 				onChangeText={(text)=>{setPostcode(text)}}
@@ -43,6 +76,16 @@ const SearchScreen = ({ navigation }) => {
 				title="送信"
 				onPress={() => {getPost()}}
 			/>
+			{isLoading ? (<Text></Text>) : (
+        
+        <Text>{data[0].address1+data[0].address2}の近くのパン屋</Text>
+				
+      )
+			}
+			{nearbakeries.map((b) => (
+        <Text>{b.name}</Text>
+      ))}
+
 		</View>
 	);
 }
