e21bd935fa956abdcbfa4e8ce0042754d83825f2
int-Miyuki <11010738ufufu@gmail.com>
2022-03-19 00:02:38 +0900

    expoで作動することを確認後のコミット

--- src/pages/start.js
+++ src/pages/start.js
@@ -1,75 +1,22 @@
-import React, {Component} from 'react';
-import { StyleSheet, Button, Text, View, TextInput } from 'react-native';
+import React from 'react'
+import { StyleSheet, Button, Text, View } from 'react-native'
 
 const StartScreen = ({ navigation }) => {
 	return (
-        // react nativeのFlatListを使用
-        //const RegisterScreen = ({ navigation }) => {};
-        class App extends Component {
-
-            constructor(props) {
-            super(props);
-            this.state = {
-                login_id: "",
-                password: "",
-                registered_list: [],
-            };
-            }
-  
-            render() {
-                const { login_id, password, registered_list } = this.state;
-            
-                return (
-                    <SafeAreaView style={styles.container}>
-                        <View style={styles.formGroup}>
-                            <Text style={styles.formLabel}>ログインID</Text>
-                            <TextInput
-                            style={styles.formControl}
-                            value={login_id}
-                            placeholder="ログインIDを入力してください"
-                            onChangeText={v => this.setState({ login_id: v })}
-                            />
-                        </View>
-                        <View style={styles.formGroup}>
-                            <Text style={styles.formLabel}>パスワード</Text>
-                            <TextInput
-                            style={styles.formControl}
-                            value={password}
-                            placeholder="パスワードを入力してください"
-                            onChangeText={v => this.setState({ password: v })}
-                            />
-                        </View>
-                        <Button
-                            title="ログイン"
-                            onPress={() => {
-                            const newList = registered_list.concat({ login_id: login_id, password: password });
-                            this.setState({
-                                login_id: "",
-                                password: "",
-                                registered_list: newList
-                            });
-                            }}
-                        />
-                        <FlatList
-                            style={styles.listBox}
-                            data={registered_list}
-                            renderItem={({ item }) => {
-                            return (
-                                <View style={styles.listItem}>
-                                    <Text>{item.login_id}</Text>
-                                    <Text>{item.password}</Text>
-                                </View>
-                            );
-                            }}
-                            keyExtractor={item => item.login_id}
-                        />
-                    </SafeAreaView>
-                );
-            }
-        }
+		<View style={styles.container}>
+			<Text>ログインしてください</Text>
+			<Button
+				title="Googleアカウントでログインする"
+				onPress={() => navigation.navigate('Start')}
+			/>
+      <Text>初期登録はこちらから</Text>
+			<Button
+				title="初期登録する"
+				onPress={() => navigation.navigate('Register')}
+			/>
+		</View>
 	);
 }
-
 
 const styles = StyleSheet.create({
   container: {
@@ -78,29 +25,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  formGroup: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  formLabel: {
-    paddingRight: 16,
-  },
-  formControl: {
-    height: 40,
-    width: 160,
-    padding: 8,
-    borderColor: 'gray',
-    borderWidth: 1
-  },
-  listItem: {
-    height: 64,
-    width: 200,
-    marginBottom: 16,
-    padding: 16,
-    borderColor: 'gray',
-    borderWidth: 1,
-  },
 });
 
 export default StartScreen
+
