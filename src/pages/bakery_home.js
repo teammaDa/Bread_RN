d64ee22aa0eddb6205f15e043e3c9bbaab176dc5
nanamih <hotehama.tech@gmail.com>
2022-03-19 12:10:30 +0900

    firestoreにtimestampを保存

--- src/pages/bakery_home.js
+++ src/pages/bakery_home.js
@@ -1,44 +1,61 @@
 import React from "react";
 import { StyleSheet, Button, Text, View } from "react-native";
+import firestore from "@react-native-firebase/firestore";
+//import { format } from "date-fns";
 
 //パン屋としてログイン後、Home画面。ここで焼き上がり送信もする。
 //保手濱担当画面
+
 const Bakery_HomeScreen = ({ navigation }) => {
   return (
     <View style={styles.container}>
       <Button
-        title="顧客登録画面"
-        onPress={() => navigation.navigate("Customer Register")}
-      />
-      <Button
-        title="顧客ログイン画面"
-        onPress={() => navigation.navigate("Customer Login")}
-      />
-      <Button
         title="パン屋検索画面"
-        onPress={() => navigation.navigate("Search Bakery")}
+        onPress={() => navigation.navigate("Search")}
       />
 
       <Text style={styles.Text}>(パン屋住所)の(パン屋店名)でログイン中</Text>
       <Text style={styles.Text}>パンが焼けましたか？</Text>
       <Button
         title="パンが焼けました"
-        onPress={() => navigation.navigate("Bread_is_Baked")}
+        onPress={() => {
+          const newTimestamp = firebase.firestore.Timestamp.now();
+          //Bakedコレクションに各店舗ドキュメントをつくり、そこに焼き上がり情報(タイムスタンプ)を入れる
+          firestore()
+            .collection("Baked") //Bakedコレクション
+            .doc("TokyoBakery") //パン屋名「東京ベーカリー」
+            .set({
+              //以下のデータを入れていく
+              bakeryname: "TokyoBakery", //ログイン中のパン屋名
+              bakery_id: "CA", //ログイン中のベーカリーID
+              timestamp: newTimestamp,
+            });
+        }}
       />
     </View>
   );
 };
 
+//.then(() => {
+//  console.log("Document successfully written!");
+//})
+//.catch((error) => {
+//  console.error("Error writing document: ", error);
+//})
+
+//将来的に焼き上がり情報にパンの種類を入れるようになった場合、
+//各店舗ドキュメント内に一度の焼き上がりサブコレクションを作り、
+//そこにタイムスタンプとパンの種類を入れると思う
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    color: "red",
     alignItems: "center",
     justifyContent: "center",
   },
   Text: {
-    color: "white",
+    color: "black",
     backgroundColor: "orange",
   },
 });
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,23 +1,24 @@
-import { NavigationContainer  } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-import HomeScreen from '../pages/home'
-import LoginScreen from '../pages/login'
-import SearchScreen from '../pages/search'
+import HomeScreen from "../pages/home";
+import LoginScreen from "../pages/login";
+import SearchScreen from "../pages/search";
+import Bakery_HomeScreen from "../pages/bakery_home";
 
 const Stack = createNativeStackNavigator();
 
 //export const AppContainer = createAppContainer(RootStack);
 
-
-export default function Router(){
-	return(
+export default function Router() {
+  return (
     <NavigationContainer>
       <Stack.Navigator>
-				<Stack.Screen name="Search" component={SearchScreen} />
-				<Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="BakeryHome" component={Bakery_HomeScreen} />
+        <Stack.Screen name="Search" component={SearchScreen} />
+        <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Home" component={HomeScreen} />
       </Stack.Navigator>
     </NavigationContainer>
-  )
+  );
 }
