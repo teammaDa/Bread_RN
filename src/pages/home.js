e560f956d546ed6b83878ee3ace88b2fd331f6bb
yuma533 <93819369+yuma533@users.noreply.github.com>
2022-03-19 17:57:14 +0900

    Update home.js

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,26 +1,23 @@
-import React from 'react'
-import { StyleSheet, Button, Text, View } from 'react-native'
+import React from "react";
+import { StyleSheet, Button, Text, View } from "react-native";
 
 const HomeScreen = ({ navigation }) => {
-	return (
-		<View style={styles.container}>
-			<Text>ログインしてください</Text>
-			<Button
-				title="ログイン"
-				onPress={() => navigation.navigate('Login')}
-			/>
-		</View>
-	);
-}
-
+  return (
+    <View style={styles.container}>
+      <Text>ログインしてください</Text>
+      <Button title="ログイン" onPress={() => navigation.navigate("Login")} />
+      <Button title="顧客画面へ" onPress={() => navigation.navigate("Customer_Home")} />
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
 
-export default HomeScreen
+export default HomeScreen;
