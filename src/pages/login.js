da04dc57bf2b9074d5fbda3645bdf1f7cbee4fe2
nanamih <hotehama.tech@gmail.com>
2022-03-26 03:46:03 +0900

    ログイン情報を渡しての画面遷移を実装

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,59 +1,103 @@
-import React from 'react'
-import { StyleSheet, Button, Text, View, ImageBackground, SafeAreaView } from 'react-native'
+//ログイン完了画面
+
+import React, { useEffect, useState } from "react";
+import {
+  StyleSheet,
+  Button,
+  Text,
+  View,
+  ImageBackground,
+  SafeAreaView,
+} from "react-native";
+import { useRoute } from "@react-navigation/native";
+import { firebase } from "../firebase/firebase";
 
 const LoginScreen = ({ navigation }) => {
-	return (
-		<SafeAreaView style={styles.container}>
-			<ImageBackground
-				source={{
-					uri: "https://pictkan.com/uploads/converted/15/06/12/1571213585-background-2561_1920-jNP-1920x1280-MM-100.jpg",
-				}}
-				style={styles.image}
-			>
-				<View style={styles.box1}>
+  const route = useRoute();
+  //start.jsから引き継いだlogin_idを使って 3/25
+  //route.params.login_idを使う
+  //TODO:firestoreのregisteredコレクションを検索。ログイン中のpostcodeとstorenameを取得 3/25
+  const login_id = route.params.login_id; //引き継いだ値をlogin_idに代入
+  const [isLoading, setLoading] = useState(true);
+  const [Loggedin, setLoggedin] = useState([]);
 
-				<Text style={styles.textWhite}>ログインしました</Text>
-				<Button
-					title="パン屋専用ホーム画面へ"
-					onPress={() => navigation.navigate('BakeryHome')}
-					color = "#F4511E"
-				/>
-				</View>
-			</ImageBackground>
-		</SafeAreaView>
-	);
-}
+  useEffect(() => {
+    firebase
+      .firestore()
+      .collection("registered")
+      .where("login_id", "==", login_id)
+      .limit(1)
+      .get()
+      .then((querySnapshot) => {
+        setLoggedin(
+          querySnapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
+        );
+      });
+  }, []);
 
+  console.log(Loggedin.map((task) => task.storename));
+  console.log(Loggedin.map((task) => task.postcode));
+
+  //registerdコレクションに入っている店舗情報のうちstorenameとpostcodeだけ代入
+  let storename = Loggedin.map((task) => task.storename);
+  let postcode = Loggedin.map((task) => task.postcode);
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <ImageBackground
+        source={{
+          uri: "https://pictkan.com/uploads/converted/15/06/12/1571213585-background-2561_1920-jNP-1920x1280-MM-100.jpg",
+        }}
+        style={styles.image}
+      >
+        <View style={styles.box1}>
+          <Text style={styles.textWhite}>ログインしました</Text>
+          <Button
+            title="パン屋専用ホーム画面へ"
+            //TODO: 第二引数にpostcodeとstorenameを入れ、bakery_home.jsに渡す 3/25
+            onPress={() =>
+              navigation.navigate("BakeryHome", {
+                storename: storename[0],
+                postcode: postcode[0],
+              })
+            }
+            color="#F4511E"
+          />
+        </View>
+      </ImageBackground>
+    </SafeAreaView>
+  );
+};
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		flexDirection: 'column',
-		textAlign: 'center',
-		justifyContent: 'center',
-	  },
-	  image: {
-		flex: 1,
-		resizeMode: "cover",
-		justifyContent: "center",
-	  },
-	  box1:{
-		backgroundColor:"#48484866",
-		width:"50%",
-		height:"10%",
-		borderBottomLeftRadius: 7,
-		borderBottomRightRadius: 7,
-		borderTopLeftRadius: 7,
-		borderTopRightRadius: 7,
-		marginLeft: 'auto',
-		marginRight: 'auto',
-		marginTop: 'auto',
-		marginBottom: 'auto',
-		alignItems:'center',
-	  },
-	  textWhite:{
-		color:"#FAFAFA"
-	  }
-	});
+  container: {
+    flex: 1,
+    flexDirection: "column",
+    textAlign: "center",
+    justifyContent: "center",
+  },
+  image: {
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "center",
+  },
+  box1: {
+    backgroundColor: "#48484866",
+    width: "50%",
+    height: "10%",
+    borderBottomLeftRadius: 7,
+    borderBottomRightRadius: 7,
+    borderTopLeftRadius: 7,
+    borderTopRightRadius: 7,
+    marginLeft: "auto",
+    marginRight: "auto",
+    marginTop: "auto",
+    marginBottom: "auto",
+    alignItems: "center",
+  },
+  textWhite: {
+    color: "#FAFAFA",
+  },
+});
 
-export default LoginScreen
+export default LoginScreen;
--- src/pages/start.js
+++ src/pages/start.js
@@ -1,138 +1,155 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 // import {Useeffect} from "react";
-import { StyleSheet,Text, View, TextInput, Button, ImageBackground, SafeAreaView} from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  ImageBackground,
+  SafeAreaView,
+} from "react-native";
 import { firebase } from "../firebase/firebase";
+
 const StartScreen = ({ navigation }) => {
-	const [ login_id, setLogin_id ] = useState("");
-	const [ password, setPassword ] = useState("");
+  const [login_id, setLogin_id] = useState("");
+  const [password, setPassword] = useState("");
 
-	
-	async function toggleLogin() {
-		console.log("in toggleLogin()")
-		
-		// firestore内に、入力されたlogin_idとpasswordが登録されているかチェック
-		const usersRef = firebase.firestore().collection("registered").where("login_id", "==", login_id ).where("password", "==", password).limit(1)
-		usersRef
-		.get()
-		.then((docSnapshot) => {
-			const docs = docSnapshot.docs;
-			// 登録されている場合 => ログイン完了画面(login.js)へ
-			if (docs.length != 0){
-				docSnapshot.forEach(doc => {
-				console.log(`${doc.id}: ${doc.data().login_id}`);
-				return (navigation.navigate('Login'));
-				})
-				// 登録されていない場合は、登録する必要があるため、初期登録画面(register.js)へ
-			} else {
-				return (navigation.navigate('Search'))	
-			}
-		})
-	}
+  async function toggleLogin() {
+    console.log("in toggleLogin()");
 
-	const testLogin = async () => {
-		for(var i=0;i<login_id.length;i++){
-			toggleLogin(login_id[i].login_id);
-		}
-	}
+    // firestore内に、入力されたlogin_idとpasswordが登録されているかチェック
+    const usersRef = firebase
+      .firestore()
+      .collection("registered")
+      .where("login_id", "==", login_id)
+      .where("password", "==", password)
+      .limit(1);
+    usersRef.get().then((docSnapshot) => {
+      const docs = docSnapshot.docs;
+      // 登録されている場合 => ログイン完了画面(login.js)へ
+      if (docs.length != 0) {
+        docSnapshot.forEach((doc) => {
+          console.log(`${doc.id}: ${doc.data().login_id}`);
 
-	return (
-				<SafeAreaView style={styles.container}>
-					<ImageBackground
-						source={{
-							uri: "https://pictkan.com/uploads/converted/15/06/12/1571213585-background-2561_1920-jNP-1920x1280-MM-100.jpg",
-						}}
-						style={styles.image}
-					>
-						<View style={styles.box1}>
-							<Text style={styles.formLabel}>ログインid</Text>
-							<View style={styles.formGroup}>
-								<TextInput
-									style={styles.formControl}
-									placeholder="login_id"
-									onChangeText={(text) => setLogin_id(text)}
-								/>
-							</View>
-							<Text style={styles.formLabel}>パスワード</Text>
-							<View style={styles.formGroup}>
-								<TextInput
-									style={styles.formControl}
-									placeholder="password"
-									onChangeText={(text) => setPassword(text)}
-								/>
-							</View>
-							<Button title="ログイン"
-								onPress={() => {testLogin()}}
-								color = "#F4511E"/>
-							
-							<Text style={styles.formLabel}>新規登録はこちらから</Text>
-							<View style={styles.formGroup}>
-								<Button title = "新規登録する"
-									onPress={() => navigation.navigate('Search')}
-									color = "#F4511E"/>
-							</View>
-						</View>
-					</ImageBackground>
-				</SafeAreaView>
-	);
-}
+          //ログイン完了画面に遷移 3/25
+          //TODO:login_idを第二引数でlogin.jsに渡す 3/25
+          //ログイン中の店舗のlogin_idを次の画面でも保持できる 3/25
+          //login_id = doc.data().login_id;
+          //return (navigation.navigate('Login'));
+          return navigation.navigate("Login", { login_id: login_id });
+        });
+        // 登録されていない場合は、登録する必要があるため、初期登録画面(register.js)へ
+      } else {
+        return navigation.navigate("Search");
+      }
+    });
+  }
 
+  const testLogin = async () => {
+    for (var i = 0; i < login_id.length; i++) {
+      toggleLogin(login_id[i].login_id);
+    }
+  };
 
+  return (
+    <SafeAreaView style={styles.container}>
+      <ImageBackground
+        source={{
+          uri: "https://pictkan.com/uploads/converted/15/06/12/1571213585-background-2561_1920-jNP-1920x1280-MM-100.jpg",
+        }}
+        style={styles.image}
+      >
+        <View style={styles.box1}>
+          <Text style={styles.formLabel}>ログインid</Text>
+          <View style={styles.formGroup}>
+            <TextInput
+              style={styles.formControl}
+              placeholder="login_id"
+              onChangeText={(text) => setLogin_id(text)}
+            />
+          </View>
+          <Text style={styles.formLabel}>パスワード</Text>
+          <View style={styles.formGroup}>
+            <TextInput
+              style={styles.formControl}
+              placeholder="password"
+              onChangeText={(text) => setPassword(text)}
+            />
+          </View>
+          <Button
+            title="ログイン"
+            onPress={() => {
+              testLogin();
+            }}
+            color="#F4511E"
+          />
 
-
-
-
+          <Text style={styles.formLabel}>新規登録はこちらから</Text>
+          <View style={styles.formGroup}>
+            <Button
+              title="新規登録する"
+              onPress={() => navigation.navigate("Search")}
+              color="#F4511E"
+            />
+          </View>
+        </View>
+      </ImageBackground>
+    </SafeAreaView>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'column',
-	textAlign: 'center',
-	justifyContent: 'center',
+    flexDirection: "column",
+    textAlign: "center",
+    justifyContent: "center",
   },
   image: {
-	flex: 1,
-	resizeMode: "cover",
-	justifyContent: "center",
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "center",
   },
-  box1:{
-	backgroundColor:"#48484866",
-	width:"50%",
-	height:"50%",
-	borderBottomLeftRadius: 7,
-	borderBottomRightRadius: 7,
-	borderTopLeftRadius: 7,
-	borderTopRightRadius: 7,
-	marginLeft: 'auto',
-	marginRight: 'auto',
-	marginTop: 'auto',
-	marginBottom: 'auto',
-	alignItems:'center',
+  box1: {
+    backgroundColor: "#48484866",
+    width: "50%",
+    height: "50%",
+    borderBottomLeftRadius: 7,
+    borderBottomRightRadius: 7,
+    borderTopLeftRadius: 7,
+    borderTopRightRadius: 7,
+    marginLeft: "auto",
+    marginRight: "auto",
+    marginTop: "auto",
+    marginBottom: "auto",
+    alignItems: "center",
   },
 
   formGroup: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
     marginBottom: 16,
   },
   formLabel: {
     paddingRight: 16,
-	marginVertical: 8,
-	color: "white"
+    marginVertical: 8,
+    color: "white",
   },
   formControl: {
     height: 40,
     width: 160,
     padding: 8,
-    borderColor: '#F4511E',
-    borderWidth: 1
+    borderColor: "#F4511E",
+    borderWidth: 1,
   },
   listItem: {
     height: 64,
     width: 200,
     marginBottom: 16,
     padding: 16,
-    borderColor: 'gray',
+    borderColor: "gray",
     borderWidth: 1,
   },
 });
