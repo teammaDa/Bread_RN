007bb8cd4da7165fd68a6684032658b0984c0d44
yuma533 <o.g.w.y.u.m2262@gmail.com>
2022-03-25 23:44:45 +0900

    ボタンの追加とスタイルの調整を行いました

--- src/pages/customer_home.js
+++ src/pages/customer_home.js
@@ -22,10 +22,6 @@ const Customer_HomeScreen = ({ navigation }) => {
   const [nearbakeries, setNearBakeries] = useState([]);
   var address = "";
 
-  const buttonAlert = () => {
-    alert("正しい郵便番号を入力してください");
-  };
-
   const getPost = async () => {
     try {
       const response = await fetch(
@@ -42,19 +38,13 @@ const Customer_HomeScreen = ({ navigation }) => {
       console.log("log");
       console.log(json.results[0].address1 + json.results[0].address2);
 
-      if (!json.results[0].address1) {
-        buttonAlert();
-      }
-
       getBakery();
-      setLoading(false);
     } catch (error) {
-      buttonAlert();
-      setLoading(true);
+      console.error(error);
     } finally {
+      setLoading(false);
     }
   };
-
   const getBakery = async () => {
     nearbakeries.length = 0;
     for (var i = 0; i < bakeries.length; i++) {
@@ -95,85 +85,87 @@ const Customer_HomeScreen = ({ navigation }) => {
 
   return (
     <View style={styles.container}>
-      <ImageBackground
-        source={{
-          uri: "https://pictkan.com/uploads/converted/15/06/12/1571213585-background-2561_1920-jNP-1920x1280-MM-100.jpg",
-        }}
-        style={styles.image}
-      >
-        <View style={styles.box1}>
-          <Text style={styles.textWhite}>
-            郵便番号を入力すると近くのパン屋を検索します
-          </Text>
-          <TextInput
-            class="textInput"
-            placeholder="郵便番号"
-            onChangeText={(text) => {
-              setPostcode(text);
-            }}
-            style={{ borderWidth: 2, borderColor: "#E11F", margin: 20 }}
-          />
+        <ImageBackground
+          source={{
+            uri: "https://pictkan.com/uploads/converted/15/06/12/1571213585-background-2561_1920-jNP-1920x1280-MM-100.jpg",
+          }}
+          style={styles.image}
+        >
+          <View style={styles.box1}>
+            <Text style={styles.Text}> </Text>
+						<Text style={styles.textWhite}>郵便番号を入力すると</Text>
+						<Text style={styles.textWhite}>近くのパン屋を検索します</Text>
+            <TextInput
+              class="textInput"
+              placeholder="郵便番号"
+              onChangeText={(text) => {
+                setPostcode(text);
+              }}
+              style={{ borderWidth: 2, borderColor: "#E11F", margin: 20 }}
+            />
 
-          <Button
-            style={styles.button}
+            <Button
+              class="button"
+              title="送信"
+							color="#F4511E"
+              onPress={() => {
+                getPost();
+              }}
+            ></Button>
+            {isLoading ? (
+            <Text> </Text>
+            ) : (
+              <Text  style={styles.textWhite}>{data[0].address1 + data[0].address2}の近くのパン屋</Text>
+            )}
+            {nearbakeries.map((b) => (
+              <Text  style={styles.textWhite}>{b.name}</Text>
+            ))}
+            <Text> </Text>
+            <Button
+              title="これまでの焼き上がり時刻を検索する"
+              onPress={() => navigation.navigate("CustomerSentResult")}
+              color = "#F4511E"
+            />
+          </View>
+          
+					<Button
             class="button"
-            title="送信"
+            title="パン屋専用ホーム画面へ"
             color="#F4511E"
-            onPress={() => {
-              getPost();
-            }}
-          ></Button>
-          {isLoading ? (
-            <Text></Text>
-          ) : (
-            <Text style={styles.textWhite}>
-              {data[0].address1 + data[0].address2}の近くのパン屋
-            </Text>
-          )}
-          {nearbakeries.map((b) => (
-            <Text style={styles.textWhite}>{b.name}</Text>
-          ))}
-        </View>
-        <Button
-          class="button"
-          title="パン屋専用ホーム画面へ"
-          color="#F4511E"
-          onPress={() => navigation.navigate("BakeryHome")} //3/19 Bakery_Homeとなっていたため保手濱がデバッグ
-        />
-      </ImageBackground>
+            onPress={() => navigation.navigate("Start")}
+          />
+          
+        </ImageBackground>
     </View>
   );
 };
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: "column",
-  },
-  image: {
-    flex: 1,
-    resizeMode: "cover",
-    justifyContent: "center",
-  },
-  box1: {
-    backgroundColor: "#48484880",
-    width: "50%",
-    height: "50%",
+	container: {
+		flex: 1,
+		flexDirection: "column"
+	},
+	image: {
+		flex: 1,
+		resizeMode: "cover",
+		justifyContent: "center"
+	},
+	box1:{
+		backgroundColor:"#48484880",
+		width:"50%",
+		height:"50%",
     borderBottomLeftRadius: 7,
     borderBottomRightRadius: 7,
     borderTopLeftRadius: 7,
     borderTopRightRadius: 7,
-    marginLeft: "auto",
-    marginRight: "auto",
-    marginTop: "auto",
-    marginBottom: "auto",
-    alignItems: "center",
-  },
-  textWhite: {
-    color: "#FAFAFA",
-  },
-  button: {
-    backgroundColor: "#FFFFFF",
-  },
-});
+		marginLeft: 'auto',
+    marginRight: 'auto',
+    marginTop: 'auto',
+    marginBottom: 'auto',
+		alignItems:'center',
+	},
+	textWhite:{
+		color:"#FAFAFA"
+	}
+ });
 
 export default Customer_HomeScreen;
